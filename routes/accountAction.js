7610875242d0ef958760574adc1e7c1cc330b43b
Xiaonan Tong <xiaonan@stanford.edu>
2014-02-21 08:45:03 +0000

    Made edits across the board.

--- routes/accountAction.js
+++ routes/accountAction.js
@@ -3,17 +3,28 @@ var mongoose = require('mongoose');
 var models = require('../models')
 
 exports.process = function(req, res){
-	if (!req.user) {
-		throw new Error('user null');
-	}
+	
 	console.log('-------USER REQUEST FROM-------');
-	console.log(req);
+	console.log(req.body);
 	console.log('-------------------------------');
+	
+	/*{ name: 'Xiaonan',
+  email: 'xiaonan@stupid.place',
+  password: 'password', points:-1 }*/
+
+	//TODO: check if account exists already. meh.
+	
+	var userJson = req.body;
+	var newUser = new models.Person(userJson);
+	
+	newUser.save(function(err, newUser){
+		console.log(newUser + " is saved!");
+		res.send(200);
+	});
 	
 	if(req.query.state == 'newaccount'){
 	  //create a family for it.
-	  	var userJson = {};
-	  	var json = {
+	  	/*var json = {
 						"controllers":[ 
 						req.user.displayName],
 						"controlees":[   ],
@@ -23,7 +34,7 @@ exports.process = function(req, res){
 			var newFamily = new models.Family(json);
 			newFamily.save(function(err, newFamily){
 			console.log(newFamily + " is saved!");
-		});
+		});*/
 	} 
 	else if (req.query.state == "adding_child")
 	{
@@ -35,5 +46,4 @@ exports.process = function(req, res){
 	  	var familyID = req.query.family;
 	  
 	}
-	res.send(20);
 }
