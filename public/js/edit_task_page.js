97eb04d49f20564f9d8a29894f6f96732ad27805
Xiaonan Tong <xiaonan@stanford.edu>
2014-02-28 00:38:00 +0000

    forgot to add some files. Now completed templating the handlebars file for TaskDetails

---
+++ public/js/edit_task_page.js
@@ -0,0 +1,38 @@
+'use strict';
+
+
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	console.log("Javascript connected!");
+	$('#submitTaskBtn').click(function(e) {
+		e.preventDefault();
+		var taskNameobj = $('#newTaskName').val();
+		var taskAssignedTo = $('input[type=radio]:checked').val()
+		if (taskAssignedTo == ""){
+			alert("Please select a child to assign the task to!");
+			return;
+		}
+		var rewardPtNum = $('#rewardVal').val();
+		var newdata = {		
+			"assignee": taskAssignedTo,
+			"taskText": taskNameobj,
+			"taskReward": parseInt(rewardPtNum),
+			"taskCompletion": 0
+		};
+		//make it into a JSON object. push it to data by calling a routes, then allow the default action to go through.
+		$.post('/callback?action=addTask', newdata, handleNewTaskCreation);
+	});
+}
+function handleNewTaskCreation(result){
+	//res.render(result)
+	alert("Task Created!");
+	window.location.href = '/';
+}
