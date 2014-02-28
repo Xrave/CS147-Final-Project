5aeab4096a02d205e74895eb2bb999710d4fbdf0
Jack Kim <jack7kim@stanford.edu>
2014-02-27 21:43:22 -0800

    finalized look of Add Task and Add Reward pages, fixed bug in Tasks page

--- views/js/edit_task_page.js
+++ views/js/edit_task_page.js
@@ -7,16 +7,22 @@ $(document).ready(function() {
 	$('.rightBtn').click(function(){
 		//replace to edit page
 		$("#blockName, #blockNumber").hide();
+		//push some stuff onto the dropdown:
+		$.get("/childList", function(res){
+			console.log(res);
+			for( i in res){
+				$("#childSelector").append("<option value='"+res[i].email"'>"+res[i].name+"</option>");
+			}
+		});
 		$(".boob").show();
-
+		
 		//replace edit button with done button
 		$("#editBtn").replaceWith("<img src='images/doneTask_btn.png' id='doneBtn' />");
 	});
 
 	$(document).on("click", "#doneBtn", function(){
-		//replace to original page
-		$("#blockName, #blockNumber").show();
-		$(".boob").hide();
-		alert("updated!");
+		//package information
+		//send all the information.
+		window.location.reload(true); 	
 	});
 })
