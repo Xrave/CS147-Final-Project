d2dd3d0decbe19f84d95f4d3696b16625de34112
Xiaonan Tong <xiaonan@stanford.edu>
2014-03-05 22:27:47 +0000

    Made edits to taskdetails page... need slimming down to be honest

--- routes/rewards.js
+++ routes/rewards.js
@@ -15,6 +15,10 @@ exports.view = function(req, res){
 	function rewardQuery(err, families) {
 		if(err) console.log(err);
 		console.log(families[0].rewards);
+        var i;
+        for(i=0; i < families[0].rewards.length; i++){
+            families[0].rewards[i].isParent = req.session.isParent;
+        }
 		//search again with the assignee email to find name
 		res.render('rewards', {"rewards": families[0].rewards});
 	}
