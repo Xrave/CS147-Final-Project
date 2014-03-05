dee430fa114b7b3828466396131ba63f93d7a002
Xiaonan Tong <xiaonan@stanford.edu>
2014-03-05 02:54:09 +0000

    fixed some more validation.

--- routes/savesettings.js
+++
@@ -1,4 +0,0 @@
-exports.view = function(req, res) { 
-   	res.render('savesettings');
-	// Your code goes here
- }
--- views/js/add_task_page.js
+++ views/js/add_task_page.js
@@ -12,18 +12,31 @@ $(document).ready(function() {
  */
 function initializePage() {
      //$("input").jqBootstrapValidation();
-
-
+    
     console.log("Javascript connected!");
-    $('#submitTaskBtn').on("tap",function(e) {
+    $('#submitTaskBtn').click(function(e) {
         e.preventDefault();
+        $.notify.defaults({elementPosition: "top center"});
         var taskNameobj = $('#newTaskName').val();
-        var taskAssignedTo = $('input[type=radio]:checked').val()
-        if (taskAssignedTo == ""){
-            alert("Please select a child to assign the task to!");
+        if(taskNameobj.length == 0){
+            $('#newTaskName').notify("Please enter a task");
             return;
         }
-        var rewardPtNum = $('#rewardVal').val();
+        var taskAssignedTo = $('input[type=radio]:checked').val()
+        if (taskAssignedTo == undefined){
+            $('#radiobuttons').notify("Please select a child to assign the task to!", "warning");
+            return;
+        }
+        var rewardPtNum = parseInt($('#rewardVal').val());
+        if( rewardPtNum<0){
+            $('#rewardVal').notify("Can't assign a negative reward", "warning");
+            return;
+        }
+        if( rewardPtNum==NaN | rewardPtNum == undefined){
+            $('#rewardVal').notify("Please enter a numerical reward value.", "warning");
+            return;
+        }
+        
         var newdata = {		
             "assignee": taskAssignedTo,
             "taskText": taskNameobj,
@@ -32,7 +45,6 @@ function initializePage() {
         };
         //make it into a JSON object. push it to data by calling a routes, then allow the default action to go through.
         $.post('/callback?action=addTask', newdata, function(e){
-            console.log("Hi");
             $.notify("Task Created!", "success");
             window.location = '/';
 
